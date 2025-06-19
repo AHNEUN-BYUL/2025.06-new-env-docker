547349a9d5586cfbf383684d0d6b9174c51b271b
eunbyul.ahn <ahneb@rsp-corp.com>
2025-06-19 18:22:51 +0900

    Test: Prettier rule 違反確認用

---
+++ frontend/src/components/TestComponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="test">
+    <p>Prettier テスト</p>
+  </div>
+</template>
+
+<script setup>
+const name = "Test User"; // ← セミコロンが付いている (semi: false 違反)
+const greeting = "Hello, " + name + "!" // ← セミコロンなし、OK
+</script>
+
+<style scoped>
+.test {
+  color: red;
+}
+</style>
