3e3edf9d94494a0c2b0fb2469d32a0a7ed680014
eunbyul.ahn <ahneb@rsp-corp.com>
2025-07-23 16:31:36 +0900

    Fix: settings.json の google-java-format 適用後の java ファイル修正

--- backend/src/main/java/com/example/demo/LambdaHandler.java
+++ backend/src/main/java/com/example/demo/LambdaHandler.java
@@ -1,18 +1,14 @@
 package com.example.demo;
 
-import java.util.Map;
-
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import java.util.Map;
 
 public class LambdaHandler implements RequestHandler<Map<String, Object>, Map<String, Object>> {
 
-    @Override
-    public Map<String, Object> handleRequest(Map<String, Object> event, Context context) {
-        context.getLogger().log("Received event: " + event);
-        return Map.of(
-            "statusCode", 200,
-            "body", "LocalStack Java Lambda からの応答です！"
-        );
-    }
+  @Override
+  public Map<String, Object> handleRequest(Map<String, Object> event, Context context) {
+    context.getLogger().log("Received event: " + event);
+    return Map.of("statusCode", 200, "body", "LocalStack Java Lambda からの応答です！");
+  }
 }
